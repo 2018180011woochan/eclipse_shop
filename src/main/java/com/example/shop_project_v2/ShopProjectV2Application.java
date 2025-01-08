9f68693807206c63a4b231ccdd789148b3266987
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2025-01-08 20:01:27 +0900

    firstCommit

---
+++ src/main/java/com/example/shop_project_v2/ShopProjectV2Application.java
@@ -0,0 +1,13 @@
+package com.example.shop_project_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopProjectV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopProjectV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shop_project_v2/ShopProjectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop_project_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopProjectV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
