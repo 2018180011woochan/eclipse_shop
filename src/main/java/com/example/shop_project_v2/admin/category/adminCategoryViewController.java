43d4b8e63e88a64a755ce8853f0490b172dc9309
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2025-01-14 20:24:33 +0900

    Feat: 카테고리 엔티티 생성

---
+++ src/main/java/com/example/shop_project_v2/admin/category/adminCategoryViewController.java
@@ -0,0 +1,14 @@
+package com.example.shop_project_v2.admin.category;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/admin/categorys")
+@RequiredArgsConstructor
+@Controller
+public class adminCategoryViewController {
+
+}
---
+++ src/main/java/com/example/shop_project_v2/category/entity/Category.java
@@ -0,0 +1,56 @@
+package com.example.shop_project_v2.category.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.example.shop_project_v2.BaseEntity;
+import com.example.shop_project_v2.product.entity.Product;
+import com.example.shop_project_v2.product.entity.ProductImage;
+import com.example.shop_project_v2.product.entity.ProductOption;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category extends BaseEntity  {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 카테고리 이름
+    @Column(nullable = false)
+    private String name;
+
+    // 메인 카테고리와 서브 카테고리 계층 구조
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent; // 상위 카테고리 (메인 카테고리)
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Category> subCategories = new ArrayList<>(); // 하위 카테고리들 (서브 카테고리)
+
+    // 유틸 메서드
+    public void addSubCategory(Category subCategory) {
+        subCategories.add(subCategory);
+        subCategory.setParent(this);
+    }
+}
