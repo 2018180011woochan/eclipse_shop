23d5ae72178aba49a11ce909567d55ca947dabcd
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2025-01-20 23:58:21 +0900

    Feat: passwordEncoder 추가

--- src/main/java/com/example/shop_project_v2/member/repository/MemberRepository.java
+++ src/main/java/com/example/shop_project_v2/member/repository/MemberRepository.java
@@ -1,5 +1,27 @@
 package com.example.shop_project_v2.member.repository;
 
-public class MemberRepository {
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.shop_project_v2.member.Membership;
+import com.example.shop_project_v2.member.Role;
+import com.example.shop_project_v2.member.entity.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+	Optional<Member> findByEmail(String email);
+	Optional<Member> findByNickname(String nickname);
+	List<Member> findAllByMembership(Membership membership);
+
+	boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+    
+    long countByRole(Role role);
+    
+    // withdraw가 true이면서 withdrawDate가 주어진 날짜 이전인 회원 찾기
+    List<Member> findByWithdrawTrueAndWithdrawDateBefore(LocalDateTime dateTime);
 }
--- src/main/java/com/example/shop_project_v2/security/SecurityConfig.java
+++ src/main/java/com/example/shop_project_v2/security/SecurityConfig.java
@@ -10,14 +10,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
+//	private final JwtFilter jwtFilter;
+//	private final CustomOAuth2UserService customOAuth2UserService;
+//	private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+//	private final CustomLogoutSuccessHandler customLogoutSuccessHandler;
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
