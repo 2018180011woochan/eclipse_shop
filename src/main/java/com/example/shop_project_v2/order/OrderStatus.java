5cb5cc6fad31258ced625f318c348ca6619611bd
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2025-01-25 00:42:14 +0900

    Feat: 주문상태 추가

---
+++ src/main/java/com/example/shop_project_v2/order/OrderStatus.java
@@ -0,0 +1,18 @@
+package com.example.shop_project_v2.order;
+
+public enum OrderStatus {
+    NEW("신규주문"), PREPARING("배송 준비 중"), IN_SHIPPING("배송중"), ARRIVE("배송 완료"),
+    CANCEL_REQUIRE("취소 요청"), REFUND_REQUIRE("환불 요청"), EXCHANGE_REQUIRE("교환 요청"), FAIL("결제 실패"), CONFIRMED("구매 확정"),
+    REFUND("환불 처리 완료"), EXCHANGE("교환 처리 완료");
+
+    private final String value;
+
+    OrderStatus(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
--- src/main/java/com/example/shop_project_v2/order/entity/Order.java
+++ src/main/java/com/example/shop_project_v2/order/entity/Order.java
@@ -1,16 +1,25 @@
 package com.example.shop_project_v2.order.entity;
 import lombok.*;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.example.shop_project_v2.BaseEntity;
+import com.example.shop_project_v2.member.Membership;
+import com.example.shop_project_v2.member.Role;
+import com.example.shop_project_v2.order.OrderStatus;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 
 @Getter
@@ -24,7 +33,10 @@ public class Order extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
-
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus orderStatus;
+    
     // 예: 주문자 정보(회원이라면 Member 엔티티와 연관관계를 맺을 수도 있음)
     private Long memberId;        // 간단히 memberId만 저장
     private String address;       // 배송 주소
@@ -33,6 +45,11 @@ public class Order extends BaseEntity {
     // 주문 총액 (주문 아이템 가격 합계)
     private int totalPrice;
 
+	@PrePersist
+    public void prePersist() {
+		orderStatus = OrderStatus.NEW;
+    }
+    
     // 주문 아이템 목록
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> orderItems = new ArrayList<>();
@@ -42,9 +59,9 @@ public class Order extends BaseEntity {
         orderItem.setOrder(this);
     }
 
-    /**
-     * 주문 총액 계산 로직 (예시)
-     */
+    public void updateStatus(OrderStatus status) {
+        orderStatus = status;
+    }
     public void calculateTotalPrice() {
         this.totalPrice = orderItems.stream()
                                     .mapToInt(OrderItem::getTotalPrice)
